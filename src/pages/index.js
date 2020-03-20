421d1c8ee80cdf85604d26390af9b67ca23414ad
LeeSeungWoo <smoothlee7104@gmail.com>
2020-03-20 15:44:54 +0900

    Remove Emoji component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { PostList } from '../components/home/PostList'
-import { Emoji } from '../components/common/Emoji'
 
 const IndexPage = () => {
   return (
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { TagList } from '../components/tags/TagList'
-import { Emoji } from '../components/common/Emoji'
 
 const TagsPage = () => {
   return (
